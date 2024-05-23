6df7ba433a46b1e70204da33da1ea379f9d46ed1
AbiBayu23 <148162535+AbiBayu23@users.noreply.github.com>
2024-05-23 18:59:27 +0700

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -3,11 +3,12 @@
 $server = "localhost";
 $user = "root";
 $password = "Perkasa23@rcm";
-$nama_database = "Halo Petani";
+$nama_database = "halopetani";
+$noHP="no_HP";
 
 
 // Buat koneksi ke database
-$conn = new mysqli($servername, $username_db, $password_db, $dbname);
+$conn = new mysqli($server, $user, $password, $nama_database);
 
 // Periksa koneksi
 if ($conn->connect_error) {
@@ -15,16 +16,16 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
+    $user = $_POST['username'];
+    $noHP = $_POST['noHP'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     // Simpan user baru ke database
-    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $username, $email, $password);
+    $stmt = $conn->prepare("INSERT INTO pengguna (username, noHP, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $user, $noHP, $password);
 
     if ($stmt->execute()) {
-        echo "Registration successful. You can now <a href='index.html'>login</a>.";
+        echo "Registration successful. You can now <a href='login.html'>login</a>.";
     } else {
         echo "Error: " . $stmt->error;
     }
