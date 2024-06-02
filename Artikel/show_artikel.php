c8b6647d76e1b675332152806df08388db5bb66e
AbiBayu23 <148162535+AbiBayu23@users.noreply.github.com>
2024-06-02 12:07:39 +0700

    Update show_artikel.php

--- Artikel/show_artikel.php
+++ Artikel/show_artikel.php
@@ -13,10 +13,10 @@
             <div class="menu">
                 <ul>
                     <li><a href="../Dashboard/DashboardAdmin.html" class="tbl-biru">Beranda</a></li>
-                    <li><a href="#pertanyaan" class="tbl-biru">Pertanyaan</a></li>
+                    <li><a href="../Pertanyaan Seputar Petani/Daftaradmin.php" class="tbl-biru">Pertanyaan</a></li>
                     <li><a href="show_artikel.php" class="tbl-biru">Artikel</a></li>
                     <li><a href="#laporan" class="tbl-biru">Laporan</a></li>
-                    <li><a href="../Login.html" class="tbl-biru">Log Out</a></li>
+                    <li><a href="../Login/Login.html" class="tbl-biru">Log Out</a></li>
                 </ul>
             </div>
         </div>
@@ -38,10 +38,37 @@
         </section>
     </div>
 
+    
     <section>
-        
-    </section>
+    <div class="container">
+    <?php
+    include '../Login/config.php';
 
+    // Query untuk mengambil data artikel terbaru
+    $query = "SELECT * FROM artikel ORDER BY tanggal_posting DESC ";
+    $result = mysqli_query($conn, $query);
+
+    // Periksa apakah query berhasil dieksekusi
+    if (!$result) {
+        echo "Query error: " . mysqli_error($conn);
+        exit();
+    }
+
+    // Ambil data dan tampilkan di web
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div>";
+        echo "<h2>" . $row['judul'] . "</h2>";
+        echo "<p>" . $row['konten'] . "</p>";
+        echo "<p>Tanggal Publikasi: " . $row['tanggal_posting'] . "</p>";
+        echo "</div>";
+    }
+
+    // Tutup koneksi
+    mysqli_close($conn);
+    ?>
+    </div>
+    </section>
+    
     <footer id="kontak">
         <div class="wrapper">
             <div class="footer-container">
