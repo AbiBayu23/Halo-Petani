876aac8db0bedbe9c5a54c698f5be04e3dca164a
AbiBayu23 <148162535+AbiBayu23@users.noreply.github.com>
2024-06-02 12:14:53 +0700

    Update sign_up.php

--- Login/sign_up.php
+++ Login/sign_up.php
@@ -1,15 +1,9 @@
 <?php
-// Konfigurasi koneksi database
-$server = "localhost";
-$user = "root";
-$password = "Perkasa23@rcm";
-$nama_database = "halopetani";
+include 'config.php';
 
 
-// Buat koneksi ke database
 $conn = new mysqli($server, $user, $password, $nama_database);
 
-// Periksa koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -20,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $noHP = $_POST['noHP'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // Simpan user baru ke database
     $stmt = $conn->prepare("INSERT INTO pengguna (username, email, noHP, password) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("ssss", $user, $email, $noHP, $password);
 
